65be7f52a72171d677f604e2b2ab295de952989a
Gopi2301 <gopinaths2301@gmail.com>
2024-07-19 23:49:48 +0530

    extracted all basic details

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,11 @@
 export function extractPrice(...elements:any){
     for (const element of elements){
         const priceText = element.text().trim()
-
-        const priceMatch = priceText.match(/\d+/);
-        if (priceMatch) return priceMatch[0];
+      if(priceText) return priceText.replace(/[^\d.]/g, '')
     }
     return '';
+}
+export function extractCurrency(element:any){
+    const currencyText= element.text().trim().slice(0,1);
+    return currencyText ? currencyText : ''
 }
