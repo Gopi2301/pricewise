b91a95b16204051fa5d45d65490d84114c714e2a
Gopi2301 <gopinaths2301@gmail.com>
2024-07-20 18:25:31 +0530

    DB created ad first product added

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className='flex flex-col justify-center'>
         <p className='small-text'>
           Smart Shopping Starts Here:
-          <Image src="/assets/icons/arrow-right.svg" alt='arrow-right' width={16} height={16}/>
+          {/* <Image src="/assets/icons/arrow-right.svg" alt='arrow-right' width={16} height={16}/> */}
         </p> 
         <h1 className='head-text'>Unleash the Power of<span className='text-primary'> PriceWise</span></h1>
         <p className='mt-6 '>Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.</p>
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,7 +1,10 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper"
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeandStoreProduct(productUrl:string){
     if(!productUrl) return
@@ -10,6 +13,28 @@ export async function scrapeandStoreProduct(productUrl:string){
         const scrapedProduct = await scrapeAmazonProduct(productUrl)
         if (!scrapedProduct) return;
         let product = scrapedProduct
+
+        const existingProduct = await Product.findOne({url:scrapedProduct.url})
+        if (existingProduct){
+            const UpdatedPriceHistory : any=[
+                ...existingProduct.priceHistory,
+                {price: scrapedProduct.currentPrice}
+            ]
+            product={
+                ...scrapedProduct,
+                priceHistory:UpdatedPriceHistory,
+                lowestPrice:getLowestPrice(UpdatedPriceHistory),
+                highestPrice:getHighestPrice(UpdatedPriceHistory),
+                averagePrice:getAveragePrice(UpdatedPriceHistory)
+            }
+        }
+        const newProduct = await Product.findOneAndUpdate(
+            {url: scrapedProduct.url},
+            product,
+            {upsert:true, new:true}
+        );
+        revalidatePath(`/products/${newProduct._id}`);
+        
     } catch (error:any) {
         throw new Error(`Failed to create/update product: ${error.message}`)
     }
